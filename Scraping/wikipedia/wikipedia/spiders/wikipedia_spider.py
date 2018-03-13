a01c0458e9543c5444f4e5f9b020358ea1bd0b47
Bulent Ozel <bulentozel@gmail.com>
2018-03-13 17:26:58 +0100

    Paramterizing the scraper.

--- Scraping/wikipedia/wikipedia/spiders/wikipedia_spider.py
+++ Scraping/wikipedia/wikipedia/spiders/wikipedia_spider.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy, re, csv
+import sys
+import os.path
+
 
 class WikipediaSpider(scrapy.Spider):
     name = "wikipedia"
     depth = 1
+    document_id = 1
     SiteStopWords = [
         'navigation',
         'search',
@@ -26,11 +30,11 @@ class WikipediaSpider(scrapy.Spider):
         Args:
             urlfile (str): the name of external file.
             
-            Note that each row should have a theme identifier, depth and url.
+            Note that each row should have a theme identifier, depth, theme name and url.
             Format:
-            0, 1, https://en.wikipedia.org/wiki/Do_it_yourself,
-            0, 1, https://en.wikipedia.org/wiki/Open_design,
-            1, 2, https://en.wikipedia.org/wiki/ustainability
+            0, 1, diy, https://en.wikipedia.org/wiki/Do_it_yourself,
+            0, 1, diy, https://en.wikipedia.org/wiki/Open_design,
+            1, 2, sustainability, https://en.wikipedia.org/wiki/sustainability
 
         Returns:
             A python list of tuples with theme Id and the url.
@@ -39,45 +43,59 @@ class WikipediaSpider(scrapy.Spider):
         with open(urlfile) as csvfile:
             urls = list()
             urlreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            next(urlreader, None)
             for row in urlreader:
                 themeid = int(row[0].strip())
                 max_depth = int(row[1].strip())
-                url = row[2].strip()
-                newroot = (themeid,max_depth,url)
+                theme = row[2].strip()
+                url = row[3].strip()
+                newroot = (themeid,max_depth,theme,url)
                 urls.append(newroot)
             return urls
                 
     def start_requests(self):
         """
         Example input format:
-        [theme_id, depth_of_crawling, seed_url]
+        [theme_id, depth_of_crawling, category_name, seed_url]
         urls = [
-            0, 1, 'https://en.wikipedia.org/wiki/Do_it_yourself'
+            0, 1, DIY, 'https://en.wikipedia.org/wiki/Do_it_yourself'
         ]
         """
-        infile = "/Users/bulentozel/OpenMaker/GitHub/OpenMaker/Scraping/wikipedia/input_urls.txt"
+        infile = getattr(self, 'infile', None)
+        if infile is None:
+            message = 'An input file with seed url is needed!\n Example run:\n scrapy crawl wikipedia -o test.json -a infile="./input_urls.txt"'
+            print(message)
+            raise scrapy.exceptions.CloseSpider(reason='cancelled')
+            
         urls = self.read_root_urls(infile)
         
         for entry in urls:
-            url = entry[2]
+            url = entry[3]
             print(url)
-            theme = entry[0]
+            themeid = entry[0]
+            theme = entry[2]
             max_depth = entry[1]
             request = scrapy.Request(url=url, callback=self.parse)
             request.meta['depth'] = 0
             request.meta['theme'] = theme
+            request.meta['theme_id'] = themeid
             request.meta['max_depth'] = max_depth
             yield request
             
     def parse(self, response):
         depth = response.meta['depth']
         theme = response.meta['theme']
+        themeid = response.meta['theme_id']
         max_depth = response.meta['max_depth']
         url = response.url
         title = response.xpath('//title/text()').re('(.*) - (.*)')[0]
         text = self._render_text(response)
+        docid = self.document_id
+        self.document_id += 1
         yield {
-            'theme.id': theme, 
+            'theme': theme,
+            'theme.id': themeid,
+            'document.id': docid,
             'title': title,
             'url': response.url,
             'depth': depth,
@@ -94,18 +112,28 @@ class WikipediaSpider(scrapy.Spider):
             request = scrapy.Request(next_page, callback=self.parse)
             request.meta['depth'] = depth
             request.meta['theme'] = theme
+            request.meta['theme_id'] = themeid
             request.meta['max_depth'] = max_depth
             yield request
             
     def _extract_links(self, response):
-        """ This part aims to extract links within the section called. 'See also'.
+        """ This part aims to extract wiki links within after the section called 'See also and
+            before the section called References.
 
         TO DO:
             - Needs to be revised via more tests.
         """
         before_References = response.xpath('//h2/span[@id="References"]/preceding::ul/li/a[@title | @href]/@href').re('/wiki/.*')
         after_See_also = response.xpath('//h2/span[@id="See_also"]/following::ul/li/a[@title | @href]/@href').re('/wiki/.*')
-        return list(set(before_References).intersection(set(after_See_also)))
+
+        see_also = list(set(before_References).intersection(set(after_See_also)))
+        if see_also:
+            return see_also
+        else:
+            after_References = response.xpath('//h2/span[@id="References"]/following::ul/li/a[@title | @href]/@href').re('/wiki/.*')
+            before_See_also = response.xpath('//h2/span[@id="See_also"]/preceding::ul/li/a[@title | @href]/@href').re('/wiki/.*')
+            return list(set(after_References).intersection(set(before_See_also)))
+        
 
     def _render_text(self, response):
         """ It extracts the main body of the article as well as text used under images, tables, etc. and cleans the text.

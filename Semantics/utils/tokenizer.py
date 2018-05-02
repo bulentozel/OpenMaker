4bbe36b665e1e582c152c05c89d24e50b26940aa
Bulent Ozel <bulentozel@gmail.com>
2018-05-02 17:41:35 +0200

    Backing up refactoring so far.

--- Semantics/utils/tokenizer.py
+++ Semantics/utils/tokenizer.py
@@ -1,7 +1,20 @@
+# -*- coding: utf-8 -*-
+"""OpenMaker text tokenizer.
+
+Author: Bulent Ozel
+e-mail: bulent.ozel@gmail.com
+
+The module contains a set of basic tools in order to tokenize a given inout text.
+
+Todo:
+    * Nothing at the moment ;)
+
+"""
+
 import re
 
-ALLOWED_SYMBOLS = list("abcdefghijklmnopqrstuvwxyz1234567890")
-CHARACTERS_TO_SPLIT = """.,():;!?\n`'-"""
+ALLOWED_SYMBOLS = list("abcdefghijklmnopqrstuvwxyz1234567890-")
+CHARACTERS_TO_SPLIT = """.,():;!?\n`'=+/\[]}{|><@#$%^&*_"""
 CHARACTERS_TO_SPLIT += '‘'+'’'+'“'+'”'+'.'
 REPLACEMENTS = {
     r"\x05": " ",
@@ -11,55 +24,22 @@ REPLACEMENTS = {
     r"-": " "
 }
 
-
-def tokenize_strip_non_words(raw):
-    """
-    Same as tokenize_words, but also removes non-word characters
-    """
-    return [t for t in tokenize_words(raw) if t not in
-            CHARACTERS_TO_SPLIT]
-
-
-def normalise(s):
-    """
-    Basic string normalisation
-
-    :param s: input string
-    :return: normalised string
-    """
- 
-    s = s.encode("ascii", "ignore").decode("ascii")
-    s = s.lower()
-    s = s.strip()
-    s = re.sub(r"\n\n+", "\n\n", s)
-    s = re.sub(r"\.\.\.+", "...", s)
-
-    for k, v in REPLACEMENTS.items():
-        s = s.replace(k, v)
-
-    symbols = set(s)
-    for c in symbols:
-        if c not in ALLOWED_SYMBOLS:
-            s = s.replace(c, " ")
-
-    return s
-
-
-def tokenize_words(raw):
-    """
-    Tokenizes strings by splitting them on spaces, line breaks or characters
-    in CHARACTERS_TO_SPLIT.
-
-    :param raw: string to split
-    :return: list of tokens
+def tokenize(raw):
+    """The function tokenizes by splitting them on spaces, line breaks or characters
+        in CHARACTERS_TO_SPLIT.
+    
+    Args:
+        raw: (:obj:`str`): Input string to split
+    
+    Returns:
+        (:obj:`list` of :obj:`str`): list of terms 
+    
     """
     tokenized = []
     temp_string = ""
     raw = normalise(raw)
-
     for cc in raw:
         c = cc
-        
         if c == " ":
             if temp_string != "":
                 tokenized.append(temp_string)
@@ -71,19 +51,57 @@ def tokenize_words(raw):
             temp_string = ""
         else:
             temp_string += c
-
     if temp_string != "":
         tokenized.append(temp_string)
-
     return tokenized
 
 
-def tokenized_pretty_print(tokens):
-    """"
-    Pretty print function for strings tokenized by tokenize_words
+def tokenize_strip_non_words(raw):
+    """Same as tokenize, but also removes non-word characters.
+    
+    Args:
+        raw: (:obj:`str`): Input string to split
+    
+    Returns:
+        (:obj:`list` of :obj:`str`): list of terms 
+        
+    """
+    return [t for t in tokenize(raw) if t not in CHARACTERS_TO_SPLIT]
 
-    :param tokens: list of tokens
-    :return: string
+
+def normalise(s):
+    """Basic string normalisation.
+    
+    Args:
+        s: (:obj:`str`): Input string to normalise.
+    
+    Returns:
+        (:obj:`str`): Normalised string.
+        
+    """
+    s = s.encode("ascii", "ignore").decode("ascii")
+    s = s.lower()
+    s = s.strip()
+    s = re.sub(r"\n\n+", "\n\n", s)
+    s = re.sub(r"\.\.\.+", "...", s)
+    for k, v in REPLACEMENTS.items():
+        s = s.replace(k, v)
+    symbols = set(s)
+    for c in symbols:
+        if c not in ALLOWED_SYMBOLS:
+            s = s.replace(c, " ")
+    return s
+
+
+def tokenized_pprint(tokens):
+    """A pretty print function for strings tokenized by tokenize.
+    
+    Args:
+        tokens: (:obj:`list` of :obj:`str`): list of terms 
+
+    Returns:
+        (:obj:`str`): The joined terms.
+        
     """
     out = ""
     for t in tokens:
